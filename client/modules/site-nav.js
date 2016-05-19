09100bf0b8b0729ae51bfd61f6ce0b5616cee5c7
Adam Havel <adam.havel@heureka.cz>
2016-05-19 17:31:01 +0200

    modified

--- client/modules/site-nav.js
+++ client/modules/site-nav.js
@@ -3,19 +3,55 @@ import component from 'component.js';
 export default function(node, selector) {
 
     const self = component(node, selector);
+    var timer = null;
 
     self.define(
         {
+            name: 'self',
+            handlers: {
+                mouseleave: function() {
+                    timer = setTimeout(function() {
+                        disableActiveSubNav();
+                        timer = null;
+                    }, 500);
+                }
+            }
+        },
+        {
             name: 'item',
+            isCollection: true,
+            handlers: {
+                mouseover: function(ev, index) {
+                    let subNav = self.element('subnav').get(index);
+
+                    if (timer) {
+                        clearTimeout(timer);
+                        timer = null;
+                    }
+
+                    disableActiveSubNav();
+                    subNav && subNav.classList.add('is-active');
+                }
+            }
+        },
+        {
+            name: 'subnav',
             isCollection: true
         }
     );
 
-    (function init() {
+    function disableActiveSubNav() {
+        let activeSubNav = null;
 
+        self.element('subnav').get().some(subNav => {
+            if (subNav.classList.contains('is-active')) {
+                activeSubNav = subNav;
+                return true;
+            }
+        });
 
-    })();
+        activeSubNav && activeSubNav.classList.remove('is-active');
+    }
 
     return self;
-
 }
